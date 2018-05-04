88b6090c33e6309f74824154ef1156512b750022
Quoc Viet <vietpnq@imt-soft.com>
2018-05-04 16:31:12 +0700

    up code

--- QLTiemNet/Models/Model1.cs
+++ QLTiemNet/Models/Model1.cs
@@ -1,4 +1,4 @@
-namespace QLTiemNet.Models
+﻿namespace QLTiemNet.Models
 {
     using System;
     using System.Data.Entity;
@@ -11,6 +11,29 @@ namespace QLTiemNet.Models
         public String Name { get; set; }
         public String UserName { get; set; }
         public String Password { get; set; }
+        public String Phone { get; set; }
+        public int Diamond { get; set; }
 
     }
-}
+
+    public class Computer
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int Status { get; set; }
+        public DateTime TimeRemaining { get; set; }
+
+        ///thuộc tính khoá ngoại
+        public int UserId { get; set; }
+    }
+
+    public class QLTiemNetDB : DbContext
+    {
+        public QLTiemNetDB(): base("name=QLTiemNetString")
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Computer> Computers { get; set; }
+    }
+} 
