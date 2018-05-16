65ca5b10df06b4ba02818a5ef677a1c7ad5ade8b
Quoc Viet <vietpnq@imt-soft.com>
2018-05-16 16:49:24 +0700

    add page Scheduler(index, edit. detail) in admin page add page Bill(index, create, detail) in admin page

---
+++ QLTiemNet/Controllers/SchedulersController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QLTiemNet.Models;
+
+namespace QLTiemNet.Controllers
+{
+    public class SchedulersController : Controller
+    {
+        private QLTiemNetDBEntities db = new QLTiemNetDBEntities();
+
+        // GET: Schedulers
+        public ActionResult Index()
+        {
+            var schedulers = db.Schedulers.Include(s => s.Computer).Include(s => s.Status).Include(s => s.User);
+            return View(schedulers.ToList());
+        }
+
+        // GET: Schedulers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Scheduler scheduler = db.Schedulers.Find(id);
+            if (scheduler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scheduler);
+        }
+
+        // GET: Schedulers/Create
+        public ActionResult Create()
+        {
+            ViewBag.ComputerId = new SelectList(db.Computers, "Id", "Name");
+            ViewBag.StatusId = new SelectList(db.Status, "Id", "Name");
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Name");
+            return View();
+        }
+
+        // POST: Schedulers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Time,UserId,ComputerId,StatusId")] Scheduler scheduler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Schedulers.Add(scheduler);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ComputerId = new SelectList(db.Computers, "Id", "Name", scheduler.ComputerId);
+            ViewBag.StatusId = new SelectList(db.Status, "Id", "Name", scheduler.StatusId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Name", scheduler.UserId);
+            return View(scheduler);
+        }
+
+        // GET: Schedulers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Scheduler scheduler = db.Schedulers.Find(id);
+            if (scheduler == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ComputerId = new SelectList(db.Computers, "Id", "Name", scheduler.ComputerId);
+            ViewBag.StatusId = new SelectList(db.Status, "Id", "Name", scheduler.StatusId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Name", scheduler.UserId);
+            return View(scheduler);
+        }
+
+        // POST: Schedulers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Time,UserId,ComputerId,StatusId")] Scheduler scheduler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(scheduler).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ComputerId = new SelectList(db.Computers, "Id", "Name", scheduler.ComputerId);
+            ViewBag.StatusId = new SelectList(db.Status, "Id", "Name", scheduler.StatusId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Name", scheduler.UserId);
+            return View(scheduler);
+        }
+
+        // GET: Schedulers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Scheduler scheduler = db.Schedulers.Find(id);
+            if (scheduler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scheduler);
+        }
+
+        // POST: Schedulers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Scheduler scheduler = db.Schedulers.Find(id);
+            db.Schedulers.Remove(scheduler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
