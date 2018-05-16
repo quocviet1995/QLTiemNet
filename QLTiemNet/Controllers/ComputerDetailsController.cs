58f3c0f3a717be7e4e759f53c549bdcc8e8a6e01
Quoc Viet <vietpnq@imt-soft.com>
2018-05-16 16:02:44 +0700

    add page ComputerDetail (index, create, edit. detail, delete) in admin page

---
+++ QLTiemNet/Controllers/ComputerDetailsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QLTiemNet.Models;
+
+namespace QLTiemNet.Controllers
+{
+    public class ComputerDetailsController : Controller
+    {
+        private QLTiemNetDBEntities db = new QLTiemNetDBEntities();
+
+        // GET: ComputerDetails
+        public ActionResult Index()
+        {
+            return View(db.ComputerDetails.ToList());
+        }
+
+        // GET: ComputerDetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ComputerDetail computerDetail = db.ComputerDetails.Find(id);
+            if (computerDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(computerDetail);
+        }
+
+        // GET: ComputerDetails/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ComputerDetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Cpu,Ram,HardDisk,Graphic,Monitor")] ComputerDetail computerDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ComputerDetails.Add(computerDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(computerDetail);
+        }
+
+        // GET: ComputerDetails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ComputerDetail computerDetail = db.ComputerDetails.Find(id);
+            if (computerDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(computerDetail);
+        }
+
+        // POST: ComputerDetails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Cpu,Ram,HardDisk,Graphic,Monitor")] ComputerDetail computerDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(computerDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(computerDetail);
+        }
+
+        // GET: ComputerDetails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ComputerDetail computerDetail = db.ComputerDetails.Find(id);
+            if (computerDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(computerDetail);
+        }
+
+        // POST: ComputerDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ComputerDetail computerDetail = db.ComputerDetails.Find(id);
+            db.ComputerDetails.Remove(computerDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
